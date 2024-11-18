4df0ee7423fac0360f3e438d9d5227309b8a03ef
messijay09 <746467@student.dusd.net>
2024-11-18 10:20:44 -0800

    add enemies
    added an damage component to my bot and movement

---
+++ 2DBegginerAdventureP2Medina/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2d;
+
+    float timer;
+    int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * direction;
+        }
+        
+
+        rigidbody2d.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        duckocontroller player = other.gameObject.GetComponent<duckocontroller>();
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+}
---
+++ 2DBegginerAdventureP2Medina/Assets/Scripts/duckocontroller1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class duckocontroller
+{
+    internal void ChangeHealth(int v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void ChnageHealth(int v)
+    {
+        throw new NotImplementedException();
+    }
+}
